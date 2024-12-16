6cdb1d3dba204bd94a50c19fe5a93073aaf12bb7
lycpaul <ycleepaul@gmail.com>
2024-12-17 01:55:12 +0800

    fixed rating reset

--- recommender.py
+++ recommender.py
@@ -1,25 +1,65 @@
 import pandas as pd
 import requests
+import numpy as np
 
 
 class Recommender:
     def __init__(self):
-        # weighted ratings
-        self.ratings = pd.read_csv('matrix/popularity.csv')
+        # normalized ratings
+        self.popularity = pd.read_csv('matrix/popularity.csv')
         self.genres = list(
-            sorted(set([genre for genre in self.ratings.Genres.unique()])))
+            sorted(set([genre for genre in self.popularity.Genres.unique()])))
+
+        # similarity matrix
+        self.similarity = pd.read_csv(
+            'matrix/similarity_top_100.csv', index_col=0)
 
     def get_displayed_movies(self):
         # get the top 100 most popular movies
-        return self.ratings.head(100)
-
-    def get_recommended_movies(self, new_user_ratings):
-        # get the top 10 most popular movies
-        return self.ratings.head(10)
+        return self.popularity.head(100)
 
     def get_popular_movies(self, genre: str):
         # get the top 10 most popular movies by genre
-        return self.ratings[self.ratings['Genres'] == genre].head(10)
+        return self.popularity[self.popularity['Genres'] == genre].head(10)
+
+    def myIBCF(self, newuser: pd.Series) -> pd.Series:
+        n = 10  # default number of recommendations
+        w = newuser
+
+        # compute the movie recommendations
+        rated_movies = w[w.notna()].index
+        rated_matrix = (~w.isna()).astype(int)
+        w = w.fillna(0)
+        recomendations = w.dot(self.similarity) / \
+            rated_matrix.dot(self.similarity)
+        recomendations = recomendations.sort_values(ascending=False)[
+            0:n].dropna()
+
+        # supplement the recommendations with the most popular movies
+        if recomendations.size < n:
+            print("supplementing recommendations with popular movies")
+            # excluding the movies already rated by the user
+            popular_movies = self.popularity[~self.popularity["MovieID"].isin(
+                rated_movies)].head(n - recomendations.size)
+            # append the popular movies to the recomendations
+            recomendations = pd.concat([recomendations, pd.Series(data=popular_movies["rating_mean"].values,
+                                        index=popular_movies["MovieID"].values)], axis=0)
+        return recomendations
+
+    def get_recommended_movies(self, newuser):
+        print(newuser)
+
+        # prepare the new user rating vector
+        w = pd.Series(data=np.nan, index=self.similarity.columns)
+        for key, value in newuser.items():
+            w[key] = value
+        recommendations = self.myIBCF(w)
+        print(w, recommendations)
+        
+        # print the recommended movies
+        df = self.popularity[self.popularity["MovieID"].isin(
+            recommendations.index)]
+        return df
 
 
 if __name__ == "__main__":
@@ -27,4 +67,5 @@ if __name__ == "__main__":
     recommender = Recommender()
     print(recommender.get_displayed_movies())
 
-    print(recommender.ratings.head())
+    print(recommender.similarity.shape)
+    print(recommender.similarity.head())
--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,16 @@ st.set_page_config(page_title="Movie Recommender", page_icon=":movie_camera:",
                    layout="wide", initial_sidebar_state="expanded")
 
 recommender = Recommender()
+
+# Initialize session state for user ratings
+if 'user_ratings' not in st.session_state:
+    st.session_state.user_ratings = {}
+
+
+# Toggle between recommendation page and rating page
+if 'isRecommendPage' not in st.session_state:
+    st.session_state.isRecommendPage = False
+
 # Sidebar for navigation
 with st.sidebar:
     st.markdown("""
@@ -41,14 +51,11 @@ def display_movie_card(movie, with_rating=False):
             rating = st.radio("Rating", [1, 2, 3, 4, 5],
                               key=f"rating_{movie.MovieID}",
                               horizontal=True,
-                              label_visibility="collapsed")
+                              label_visibility="collapsed",
+                              index=None)
             return rating
     return None
 
-
-# Initialize session state for user ratings
-if 'user_ratings' not in st.session_state:
-    st.session_state.user_ratings = {}
 
 # System 1 - Genre
 if page == "System 1 - Genre":
@@ -69,35 +76,34 @@ elif page == "System 2 - Collaborative":
     st.title("Rate some movies!")
     movies = recommender.get_displayed_movies()
 
-    # Toggle between recommendation page and rating page
-    recommended = False
+    def reset_ratings():
+        """Callback function to reset user ratings and session state."""
+        st.session_state.isRecommendPage = False
+        st.session_state.user_ratings = {}
+        print("reset user ratings", st.session_state.user_ratings)
 
     # Only enable recommendation button if there are ratings
-    has_ratings = len(st.session_state.user_ratings) > 0
-    if st.button("Get recommendations", disabled=not has_ratings):
+    if st.button("Get recommendations"):
         # Clear the page
         st.empty()
-        recommended = True
-        # Fetch and display recommendations
-        recommended_movies = recommender.get_recommended_movies(
-            st.session_state.user_ratings)
-        st.write("Your recommendations")
+        st.session_state.isRecommendPage = True
 
-        # Display top 10 recommended movies in rows of 5
+        # Fetch and display recommendations
+        recommendations = recommender.get_recommended_movies(
+            st.session_state.user_ratings)
+
+        # Use callback for "Try Again" button
+        st.button("Try Again", on_click=reset_ratings)
+
+        # Display top 10 recommended movies
+        st.write("Your recommendations")
         rec_cols = st.columns(5)
-        for idx, movie in recommended_movies.head(10).iterrows():
+        for idx, movie in recommendations.head(10).iterrows():
             with rec_cols[idx % 5]:
                 display_movie_card(movie)
 
-        if st.button("Try Again"):
-            # Reset user ratings
-            recommended = False
-            has_ratings = False
-            st.session_state.user_ratings = {}
-            st.experimental_rerun()
-
-    if not recommended:
-        # Display movies in rows of 5
+    if not st.session_state.isRecommendPage:
+        # Display movies rating
         cols = st.columns(5)
         for idx, movie in movies.iterrows():
             with cols[idx % 5]:
